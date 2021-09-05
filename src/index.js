d952f061536bfff1b04964710969d1b14de348e4
HA Hoang <hoang.habk2002@hcmut.edu.vn>
2021-09-05 19:23:55 +0700

    them suon cho file cho project

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars  = require('express-handlebars');
+const app = express()
+const port = 3000
+
+//app.use(morgan("combined")) // track HTTP call
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname,'public'))) // set static public
+
+//templet engine
+app.engine('hbs',handlebars({
+      extname: '.hbs' // change file types name
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname,'resources/views')) // set views
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/69', (req, res) => {
+    res.render('yaesama')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
+app.get('/register',(req,res) => {
+  res.render('register')
+})
+
+app.post('/register',(req,res) => {
+  console.log(req.body)
+  if(req.body.password != req.body.cfpassword){ //document.write("not match password")
+   res.send('registered')}
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
