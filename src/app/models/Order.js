f62718538f327c73174ce28b5312cfd35e390bd5
hoangha1357 <76683867+hoangha1357@users.noreply.github.com>
2021-11-27 15:59:41 +0700

    remove view image

--- src/app/models/Order.js
+++ src/app/models/Order.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const mongoosedelete = require('mongoose-delete');
 const Schema = mongoose.Schema;
-const { ordering } = require('../controllers/UserController');
 
 const Order = new Schema(
     {
